1255c8206363c21668e38a42bcdbfc4d93dc1bf9
Becky Wu <becky.wu@twotalltotems.com>
2019-01-07 11:14:59 -0800

    recognize copy command

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
 const clear = require('clear');
+const yargs = require('yargs');
 
 const files = require('./lib/files');
 
 clear();
-
 console.log('React Native Template CLI');
 
-console.log(`Current Directory: ${files.getCurrentDirectoryName()}`);
+const argv = yargs.argv;
 
-files.copyFilesToCurrentDirectory(`${__dirname}/templates`);
+var command = argv._[0];
+console.log(`Command: ${command}`);
+
+if (command === 'copy') {
+    console.log(`Current Directory: ${files.getCurrentDirectoryName()}`);
+
+    if (!argv.type || !argv.name) {
+        files.promptRequiredFilesInfo(`${__dirname}/templates`, command);
+    } else {
+        console.log(argv.type, argv.name);
+
+        if (argv.type === 'Component' || argv.type === 'Reducer' || argv.type === 'Screen') {
+            const copy = `RN${argv.type}Template`;
+            files.copyFilesToCurrentDirectory(`${__dirname}/templates`, argv.name, copy);
+        } else {
+            console.log('Invalid Template Type.');
+            console.log('Available Template Type: `Component`, `Reducer`, `Screen`.');
+        }
+    }
+} else {
+    console.log('Unrecognized Command! ');
+    console.log('Please run `react-native-template-cli --help` to see the list of available commands.');
+}
+
+
 
